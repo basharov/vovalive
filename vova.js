a4f9fef6fb6b50b5ea10a85d749790cdbe8665d3
Sergei Basharov <sergei@basharov.net>
2023-09-12 19:24:47 +0300

    config

--- vova.js
+++ vova.js
@@ -2,6 +2,10 @@ let ctx
 let audio
 
 const playback = () => {
+  if (!ctx) {
+    ctx = new AudioContext()
+  }
+
   const playSound = ctx.createBufferSource()
   playSound.buffer = audio
   playSound.connect(ctx.destination)
@@ -10,7 +14,6 @@ const playback = () => {
 }
 
 const init = () => {
-  ctx = new AudioContext()
   fetch('assets/2.wav')
     .then(data => data.arrayBuffer())
     .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
