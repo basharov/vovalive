3086c28f878ce2131be102e50c782ddc43e4fcb4
Sergei Basharov <sergei@basharov.net>
2023-09-12 19:14:07 +0300

    Initial commit

---
+++ vova.js
@@ -0,0 +1,12 @@
+const init = () => {
+  const ctx = new AudioContext()
+  let audio
+  fetch('./assets/2.wav')
+    .then(data => data.arrayBuffer())
+    .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+    .then(decodedAudio => {
+      audio = decodedAudio
+    })
+}
+
+init()
