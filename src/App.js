bea0394288861f79318c241ffcd8841daada4825
Sergei Basharov <sergei@basharov.net>
2023-09-12 20:59:31 +0300

    cra, sounds

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css'
+import * as Tone from 'tone'
+import { useState } from 'react'
+
+const baseUrl = 'https://basharov.net/vovalive/assets'
+
+function App() {
+
+  const [sounds, setSounds] = useState(undefined)
+  const [synths, setSynths] = useState(undefined)
+
+  const loadSounds = async () => {
+    await Tone.start()
+
+    const players = new Tone.Players(
+      {
+        und1: `${baseUrl}/vova/2.wav`,
+        und2: `${baseUrl}/vova/3.wav`
+      }
+    ).toDestination()
+
+    const synths = new Tone.Players(
+      {
+        1: `${baseUrl}/synths/agressive_scream_ambient.wav`,
+      }
+    ).toDestination()
+
+    setSynths(synths)
+  }
+
+  const play = async () => {
+    await Tone.start()
+    const synth1 = synths.player('1')
+    const und1Player = sounds.player('und1')
+    const und2Player = sounds.player('und2')
+    synth1.loop = true
+    synth1.start()
+
+    // und1Player.loop = true
+    // und1Player.start()
+    // und2Player.start(`+${und1Player.buffer.duration}`)
+  }
+
+  return (
+    <div className='App'>
+      <button onClick={loadSounds}>Start</button>
+      <button onClick={play}>Play</button>
+    </div>
+  )
+}
+
+export default App
